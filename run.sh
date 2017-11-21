7a761e3f71bd35170da70de0785563ebbdb6a420
Junya Hayashi <junya.hayashi@groove-x.com>
2017-11-21 19:30:14 +0900

    "fakeroot debian/rules clean" instead of delete debian directory

--- run.sh
+++ run.sh
@@ -4,11 +4,11 @@ set -e
 
 apt --yes install devscripts fakeroot python-bloom
 
-rm -rf debian
 NOW=`date -u +'%Y%m%dT%H%M%SZ'`
 bloom-generate rosdebian \
                --os-name ${WERCKER_BLOOM_GENERATE_OS_NAME} \
                --os-version ${WERCKER_BLOOM_GENERATE_OS_VERSION} \
                --ros-distro ${WERCKER_BLOOM_GENERATE_ROS_DISTRO}
 sed -i -e "1 s/-\w*)/-${NOW})/" debian/changelog
+fakeroot debian/rules clean
 fakeroot debian/rules binary
